a4145c6a84075093d9eedeb3239af29d55798768
James Mansoor <james.mansoor@covan.com>
2022-09-06 15:53:46 -0500

    Add project files.

---
+++ FileProcessor.cs
@@ -0,0 +1,63 @@
+﻿using System.Globalization;
+using CsvHelper.Configuration;
+using CsvHelper;
+using NLog;
+
+namespace EFS_FileProcessor
+{
+    internal class FileProcessor
+    {
+    private static readonly Logger Logger = Nlog_Configuration.NlogConfiguration.Config();
+        private string InputFilePath { get; }
+        private string OutputFilePath { get; } 
+       
+
+        internal FileProcessor(string inputPath, string outputPath)
+        {
+            InputFilePath = inputPath;
+            OutputFilePath = outputPath;
+        } 
+
+        internal void Process()
+        {
+            
+            
+                using StreamReader inputReader = new StreamReader(InputFilePath);
+                using StreamWriter outputWriter = new StreamWriter(OutputFilePath);
+
+            var csvConfiguration = new CsvConfiguration(CultureInfo.InvariantCulture)
+            {
+                HasHeaderRecord = false,
+                TrimOptions = TrimOptions.Trim
+                };
+                using CsvReader csvReader = new CsvReader(inputReader, csvConfiguration);
+                using CsvWriter csvWriter = new CsvWriter(outputWriter, csvConfiguration);
+
+                IEnumerable<dynamic> records = csvReader.GetRecords<dynamic>();
+                int cardNumCount = 0;
+                foreach (dynamic record in records)
+                {
+                    if (record.Field1 == "TA")
+                    {
+                        record.Field5 = record.Field5.Substring(0, 4);
+                        cardNumCount++;
+                    }
+                        try
+                        {
+                        csvWriter.WriteRecord(record);
+                        csvWriter.NextRecord();
+                        }
+                        catch (Exception e)
+                        {
+                            Logger.Error($"Writer unable to write to {Path.GetFileName(OutputFilePath)}");
+                            Logger.Error(e);
+                        }
+
+                }
+                Logger.Info($"{Path.GetFileName(InputFilePath)} Processed to {Path.GetFileName(OutputFilePath)} with {cardNumCount} card numbers truncated");
+            
+            
+        }
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,63 @@
+﻿using NLog;
+
+
+namespace EFS_FileProcessor
+{
+    internal class Program
+    {
+        private static readonly Logger Logger = Nlog_Configuration.NlogConfiguration.Config();
+        static void Main(string[] args)
+        {
+            //pull settings from SSMS
+             using JamesDBContext context = new JamesDBContext(); 
+            string inputDir =
+                (from pa in context.PropertyApplication
+                 join p in context.Property on pa.PropertyID equals p.PropertyID
+                 join a in context.Application on pa.ApplicationID equals a.ApplicationID
+                 where a.ApplicationID == 352 //EFS application ID
+                 where p.PropertyTypeID == 10 //directory 1
+                 select p.Property1).SingleOrDefault();
+            string outputDir =
+                (from pa in context.PropertyApplication
+                join p in context.Property on pa.PropertyID equals p.PropertyID
+                join a in context.Application on pa.ApplicationID equals a.ApplicationID
+                where a.ApplicationID == 352 //EFS application ID
+                where p.PropertyTypeID == 11 //directory 2
+                select p.Property1).SingleOrDefault();
+            
+            
+            string outputFile;
+            //iterate through files and process
+            try
+            {
+            string[] filePaths = Directory.GetFiles(inputDir,$"*.csv");
+                if (filePaths.Length==0)
+                {
+                    Logger.Error("No file found in the input directory");
+                    return;
+                }
+                else
+                {
+                    for (int i = 0; i < filePaths.Length; i++)
+                    {
+                        outputFile = $"{outputDir}\\T-{DateTime.Now.ToString("yyyyMMdd", System.Globalization.CultureInfo.InvariantCulture)}({i + 1}).txt";
+                        FileProcessor processor = new FileProcessor(filePaths[i], outputFile);
+                        Logger.Info($"File {filePaths[i]} Sent for Processing");
+                        processor.Process();
+                    }
+                }
+            } 
+            catch (DirectoryNotFoundException e)
+            {
+                Logger.Error("Directory does not exist");
+                Logger.Error(e);
+            }
+            catch(ArgumentNullException e)
+            {
+                Logger.Error("Path is null");
+                Logger.Error(e);
+            }
+            
+        }
+    }
+}
